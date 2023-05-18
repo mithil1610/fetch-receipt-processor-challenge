315502e3e6f4be14ccd4a2f563f6f7c15512fcd8
mithil1610 <mpatel152@hawk.iit.edu>
2023-05-18 14:37:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,4 @@ def get_points(receipt_id):
     return jsonify(response)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
